1a43544de06457cdebaff5fd3f1845034d6bdad4
George Kinnear <G.Kinnear@ed.ac.uk>
2022-04-26 17:49:40 +0100

    remove dependency on nmmBtm

--- cj_functions.R
+++ cj_functions.R
@@ -22,18 +22,18 @@ split_halves_irr <- function(decisions_data,
     ) %>%
     mutate(winner = 1) %>% # indicate to sirt::btm that the winner is the leftmost column
     data.frame
-  
+
   # Create the judging groups - either as a split half, or with specified size
   if(is.null(judge_group_size)) {
     # Group 1: sample half of the judges at random
-    judge_group1 <- decisions_for_sirt %>% 
-      select(JudgeID) %>% 
-      distinct() %>% 
+    judge_group1 <- decisions_for_sirt %>%
+      select(JudgeID) %>%
+      distinct() %>%
       slice_sample(prop = 0.5)
     # Group 2: the remaining judges
-    judge_group2 <- decisions_for_sirt %>% 
-      select(JudgeID) %>% 
-      distinct() %>% 
+    judge_group2 <- decisions_for_sirt %>%
+      select(JudgeID) %>%
+      distinct() %>%
       anti_join(judge_group1, by = c("JudgeID"))
   } else {
     num_judges = decisions_for_sirt %>%
@@ -43,26 +43,26 @@ split_halves_irr <- function(decisions_data,
       stop(paste0("Judge group size ", judge_group_size, " not possible - there are only ", num_judges, " judges"))
     }
     # Group 1: sample the required number of judges
-    judge_group1 <- decisions_for_sirt %>% 
-      select(JudgeID) %>% 
-      distinct() %>% 
+    judge_group1 <- decisions_for_sirt %>%
+      select(JudgeID) %>%
+      distinct() %>%
       slice_sample(n = judge_group_size)
     # Group 2: sample the required number of judges from the remaining judges
-    judge_group2 <- decisions_for_sirt %>% 
-      select(JudgeID) %>% 
-      distinct() %>% 
-      anti_join(judge_group1, by = c("JudgeID")) %>% 
+    judge_group2 <- decisions_for_sirt %>%
+      select(JudgeID) %>%
+      distinct() %>%
+      anti_join(judge_group1, by = c("JudgeID")) %>%
       slice_sample(n = judge_group_size)
   }
-  
+
   judgements1 <- decisions_for_sirt %>% semi_join(judge_group1, by = c("JudgeID")) %>% select(-JudgeID)
   judgements2 <- decisions_for_sirt %>% semi_join(judge_group2, by = c("JudgeID")) %>% select(-JudgeID)
-  
+
   btm1 <- sirt::btm(judgements1 %>% data.frame , maxit=400 , fix.eta=0 , ignore.ties=TRUE )
   btm2 <- sirt::btm(judgements2 %>% data.frame , maxit=400 , fix.eta=0 , ignore.ties=TRUE )
-  
+
   merged_effects <- merge(btm1$effects, btm2$effects, by="individual")
-  
+
   # This is a simpler version which just returns the correlation coefficient:
   #return(cor(merged_effects$theta.x, merged_effects$theta.y, method="pearson"))
   return(tibble(
@@ -93,7 +93,7 @@ split_halves_irr_iterations <- function(decisions_data,
         judge_group_size
       ))
     )
-  
+
   return(iterations)
 }
 
@@ -104,56 +104,48 @@ split_halves_irr_iterations <- function(decisions_data,
 #' @param chosen_col The column in decisions_data that holds the winner of each judgement
 #' @param notchosen_col The column in decisions_data that holds the loser of each judgement
 #' @param judge_col The column in decisions_data that holds the judge identifier
-#' @param split_halves_iterations How many iterations to use for split-halves reliability
 #' @return data frame with the sirt::btm output in tidy form
 #' @import sirt
 #' @import dplyr
-btm_for_cj <- function(decisions_data, chosen_col = "Won", notchosen_col = "Lost", judge_col = "JudgeID", split_halves_iterations = 10) {
+btm_for_cj <- function(decisions_data, chosen_col = "Won", notchosen_col = "Lost", judge_col = "JudgeID") {
   # prepare the judgement data in the form required by sirt::btm
   decisions_for_sirt <- decisions_data %>%
     select(chosen = all_of(chosen_col), notChosen = all_of(notchosen_col)) %>%
     mutate(winner = 1) %>% # indicate to sirt::btm that the winner is the leftmost column
     data.frame
-  
+
   # fit the Bradley-Terry model
   mdl <- sirt::btm(decisions_for_sirt,
+                   # include judge details so that sirt::btm computes judge infit
+                   judge = decisions_data %>% pull(all_of(judge_col)),
                    maxit=400,
                    fix.eta=0, # the "home advantage" should be 0 as left column does not mean home
                    ignore.ties=TRUE)
-  
-  # find the judge infit values
-  # TODO - reconcile the approach used in nmmBtm::btm_fit_2 with the one in sirtmarkdown.Rmd that gives different results
-  judge_fits <-
-    nmmBtm::btm_fit_2(
-      mdl[["probs"]],
-      decisions_data %>% rename(judgeName = all_of(judge_col))
-    ) %>%
+
+  judge_fits <- mdl[["fit_judges"]] %>%
+    transmute(judge = as.character(judge), infit) %>%
+    setNames(c(judge_col, "infit")) %>%
     mutate(
       infit_mean = mean(infit),
       infit_sd = sd(infit),
       # note which judges have an infit value more than 2sd above the mean
       discard = infit > infit_mean + 2*infit_sd
-    ) %>% 
-    select(judgeName, contains("infit"), discard)
-  
-  # Script misfits
-  # TODO - bring this in from sirtmarkdown.Rmd
-  #      - or, is it already there in the btm_estimates? (infit/outfit cols)
-  
-  # Find the inter-rater reliability
-  # TODO - probably just get rid of this and leave it to users to add on?
-  #split_halves_iterations <-
-  #  split_halves_irr_iterations(decisions_data, iters = c(1:split_halves_iterations))
-  
+    ) %>%
+    select(all_of(judge_col), contains("infit"), discard)
+
+  # Alternativey, can compute by hand using formulae from Pollitt (2012), https://doi.org/10.1080/0969594X.2012.665354
+  # my_judge_fits <- bind_cols(mdl[["probs"]], decisions_data %>% rename(judgeName = all_of(judge_col))) %>%
+  #   mutate(sq_std_res = (1-p1)^2 / (p1*(1-p1))) %>% # Eq. 10
+  #   group_by(judgeName) %>%
+  #   summarise(infit = sum(p1*(1-p1)*sq_std_res) / sum(p1*(1-p1)), .groups = "drop") # Eq. 11
+
   return(tibble(
     N_R = nrow(mdl[["effects"]]),
     N_A = nrow(judge_fits),
     N_C = nrow(decisions_for_sirt),
     ssr = list(mdl$mle.rel),
     btm_estimates = list(as_tibble(mdl[["effects"]])),
-    #  split_halves = list(as_tibble(split_halves_iterations)),
     judge_fits = list(as_tibble(judge_fits)),
-    #num_split_halves = nrow(split_halves_iterations),
   ))
-  
+
 }
